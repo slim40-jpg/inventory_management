f5e32a40f73b0d5e38a442e69d5083224b7bafbf
slim40-jpg <slim.salmi@supcom.tn>
2025-11-05 13:32:46 +0100

    adding authroute

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const AuthController = require('../controllers/AuthController');
+//const { refreshToken } = require('../controllers/RefreshTokenController');
 
 // Example route
-router.post('/login', (req, res) => {
-  
-});
+router.post('/login', AuthController.login )
+router.post('/SignUp' ,AuthController.signUp);
+//router.post('/refresh', refreshToken);
+router.post('/logout', AuthController.logout);
+
+
 
 module.exports = router;
